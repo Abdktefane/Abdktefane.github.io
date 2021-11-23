e97c7fb43f2457bfb727b6e98a8a1854303bf85a
abd ktefane <abd.ktefane.7@gmail.com>
2021-11-23 11:11:39 +0200

    Updates

---
+++ precache-manifest.8c9d16be8efdf24c67f72e699193f436.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35c8e888575ef27a7c84f6a095f45b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c21bc59dd664489978",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "e6a2581fdb7926b89147",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "a5bf42afed4fe1aef73f",
+    "url": "/static/css/main.31d3be4c.chunk.css"
+  },
+  {
+    "revision": "780c1e13d59ea6231dc8",
+    "url": "/static/js/2.40e6a952.chunk.js"
+  },
+  {
+    "revision": "ea6bcf1255802265408f45d205d80624",
+    "url": "/static/js/2.40e6a952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c21bc59dd664489978",
+    "url": "/static/js/3.99f7b70b.chunk.js"
+  },
+  {
+    "revision": "e6a2581fdb7926b89147",
+    "url": "/static/js/4.8480fa2d.chunk.js"
+  },
+  {
+    "revision": "a5bf42afed4fe1aef73f",
+    "url": "/static/js/main.45308cfa.chunk.js"
+  },
+  {
+    "revision": "a9496fd66f8621a534cff55479dfb65e",
+    "url": "/static/js/main.45308cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87118dd7087a4e7b193f",
+    "url": "/static/js/runtime-main.469706f9.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "1664fd8ccf5e762b7451c493aaf2d9ce",
+    "url": "/static/media/FiveCLogo.1664fd8c.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f60f3cd89b4897f86af46c42fe17a422",
+    "url": "/static/media/arabicSchoolLogo.f60f3cd8.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "d4d9da71e95b005fc845c21ad032ee76",
+    "url": "/static/media/damascus_logo.d4d9da71.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "5fbab39b9e5cda16efd83cc7f02230b7",
+    "url": "/static/media/legendLogo.5fbab39b.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "e7f203efbd7bc8ec933de0f24ea8fd54",
+    "url": "/static/media/newTechLogo.e7f203ef.png"
+  },
+  {
+    "revision": "b79eb04c4497cf903fb1d398f5f5ea41",
+    "url": "/static/media/personal_profile.b79eb04c.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "efacab4a14c833d6f1bb678fecec89ae",
+    "url": "/static/media/tawreedFullLogo.efacab4a.png"
+  },
+  {
+    "revision": "8240e6baf2ec0c4f77002f5a7faba6b9",
+    "url": "/static/media/unifiyLogo.8240e6ba.jfif"
+  }
+]);
